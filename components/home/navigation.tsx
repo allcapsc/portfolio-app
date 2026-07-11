a30b34d327efebc2edd96f7de381083232f5e82c
Edgar Sanchez <65421180+allcapsc@users.noreply.github.com>
2026-07-11 15:02:44 -0700

    Update navigation.tsx

--- components/home/navigation.tsx
+++ components/home/navigation.tsx
@@ -15,14 +15,14 @@ export default function Navigation() {
         </a>
         <a href="#experience" className="font-base text-sm sm:text-base hover:text-main transition-colors">
           Experience
-        </a> */}
+        </a>
         <a 
           href="/resume" 
           target="_blank"
           className="border-border shadow-shadow rounded-base border-2 px-4 py-2 font-base text-sm sm:text-base hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none transition-all active:scale-95"
         >
           Resume
-        </a>
+        </a> */}
       </div>
     </nav>
   )
